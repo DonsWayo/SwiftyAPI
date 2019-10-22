ab3546a06c33ebd12eaa83e78681f9f7a0a949c5
Juan Jose Carracedo <juan.jose.carracedo@accenture.com>
2019-10-23 00:23:26 +0200

    Initial Commit

---
+++ SwiftyAPI/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftyAPI
+//
+//  Created by Jose Carracedo, Juan on 23/10/2019.
+//  Copyright © 2019 Jose Carracedo, Juan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
